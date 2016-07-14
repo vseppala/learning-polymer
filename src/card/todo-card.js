94d34db50a1d7754b8ecfc629e068fe16b5d3686
Ville Seppala <ville.seppala@digia.com>
2016-07-14 15:46:08 +0300

    Add functionality to mark todos as Done.

--- src/card/todo-card.js
+++ src/card/todo-card.js
@@ -1,3 +1,17 @@
 Polymer({
-  is: 'todo-card'
+  is: 'todo-card',
+
+  properties: {
+    heading: String,
+    content: String,
+    todoid: Number
+  },
+
+  listeners: {
+    'tap': 'done'
+  },
+
+  done: function (e) {
+    this.fire('todoDone', this.todoid);
+  }
 })
--- src/views/todo-list/todo-list.js
+++ src/views/todo-list/todo-list.js
@@ -1,3 +1,20 @@
 Polymer({
-  is: 'todo-list'
+  is: 'todo-list',
+
+  listeners: {
+    'todoDone': 'removeTodo'
+  },
+
+  ready: function () {
+    this.todos = [
+      {heading: 'Otsikko', content: 'Some content...'},
+      {heading: 'Otsikko', content: 'Some coasdntent...'}
+    ];
+  },
+
+  removeTodo: function (e) {
+    var todoClone = this.todos.slice(0);
+    todoClone.splice(e.detail, 1);
+    this.todos = todoClone;
+  }
 })
