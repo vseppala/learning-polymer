6b7c94e5e7487a62a46a962ed082c9d09ead33d2
Ville Seppala <ville.seppala@digia.com>
2016-07-14 16:01:45 +0300

    Add 'add new todo' functionality

--- src/my-app.js
+++ src/my-app.js
@@ -1,3 +1,33 @@
 Polymer({
-  is: 'my-app'
+  is: 'my-app',
+
+  listeners: {
+    'add-button.tap': 'addNew',
+    'todoDone': 'removeTodo'
+  },
+
+  ready: function () {
+    this.todos = [
+      {heading: 'Otsikko', content: 'Some content...'},
+      {heading: 'Otsikko', content: 'Some coasdntent...'}
+    ];
+  },
+
+  addNew: function (e) {
+    var todoClone = this.todos.slice(0);
+
+    todoClone.push({
+      heading: 'Added new one',
+      content: 'Added new todo entry with THE add button...'
+    });
+
+    this.todos = todoClone;
+  },
+
+  removeTodo: function (e) {
+    var todoClone = this.todos.slice(0);
+    todoClone.splice(e.detail, 1);
+    this.todos = todoClone;
+  }
+
 })
--- src/views/todo-list/todo-list.js
+++ src/views/todo-list/todo-list.js
@@ -1,20 +1,7 @@
 Polymer({
   is: 'todo-list',
 
-  listeners: {
-    'todoDone': 'removeTodo'
-  },
-
-  ready: function () {
-    this.todos = [
-      {heading: 'Otsikko', content: 'Some content...'},
-      {heading: 'Otsikko', content: 'Some coasdntent...'}
-    ];
-  },
-
-  removeTodo: function (e) {
-    var todoClone = this.todos.slice(0);
-    todoClone.splice(e.detail, 1);
-    this.todos = todoClone;
+  properties: {
+    todos: Object
   }
 })
